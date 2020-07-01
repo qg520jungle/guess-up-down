0980af2432bfc27060b932a62f3ffefebd0260e4
zhangxianggeng@wuxicloud.com <zhangxianggeng@wuxicloud.com>
2020-07-02 00:47:17 +0800

    初始化

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormPage from 'views/FormPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
