19f6383df1510513ef7f41a7f11e95ad0fd13985
zhangxianggeng@wuxicloud.com <zhangxianggeng@wuxicloud.com>
2020-07-03 01:43:21 +0800

    猜涨跌主页内容分布

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import FormPage from 'views/FormPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <FormPage />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">FormPage</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <FormPage />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+// eslint-disable-next-line no-unused-vars
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
--- src/views/FormPage/index.jsx
+++ src/views/FormPage/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import Form from 'components/Form';
-import Input from 'components/Input';
-// import Select from 'components/Select';
-// import Checkbox from 'components/Checkbox';
 
 function FormPage() {
   return (
     <div className="page">
-      <Form>
-        <Input />
-        {/* <Select />
-        <Checkbox /> */}
-      </Form>
+      <div>标题 猜涨跌</div>
+      <div>内容 涨跌赔率</div>
+      <div>内容 涨跌可能性</div>
+      <div>内容 涨跌期望 方差 风险指数</div>
+      <div>内容 猜涨跌 建议</div>
     </div>
   );
 }
